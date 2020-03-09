cb751a3e9a343b4719aa19b6aa48e1de109e4d3d
Rob-Swensen <rg.swensen801@gmail.com>
2020-03-09 12:05:47 -0600

    More CSS Changes

--- server/Controllers/suggestedController.js
+++ server/Controllers/suggestedController.js
@@ -4,7 +4,7 @@ module.exports = {
     getMovies: (req, res) => {
         movieList = [];
         
-        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=b0905bacefecc34fb178a826419bdf12&language=en-US&page=10`)
+        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=b0905bacefecc34fb178a826419bdf12&language=en-US&page=5`)
         .then(response => {
             movieList = [...response.data.results]
                 axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=b0905bacefecc34fb178a826419bdf12&language=en-US&page=2`)
@@ -22,7 +22,7 @@ module.exports = {
                                                 axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=b0905bacefecc34fb178a826419bdf12&language=en-US&page=7`)
                                                 .then(response => {
                                                     movieList =[...movieList, ...response.data.results]
-                                                        axios.get('https://api.themoviedb.org/3/movie/popular?api_key=b0905bacefecc34fb178a826419bdf12&language=en-US&page=9')
+                                                        axios.get('https://api.themoviedb.org/3/movie/popular?api_key=b0905bacefecc34fb178a826419bdf12&language=en-US&page=11')
                                                         .then(response => {
                                                             movieList = [...movieList, ...response.data.results]
                                                             res.status(200).send(movieList)})
--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -14,14 +14,14 @@ class Movie extends Component{
             <div className='watched-movie-container'>
                 <img className='my-poster watched-poster' src={`https://image.tmdb.org/t/p/w500${this.props.movie.poster_path}`} alt={this.props.movie.title}/>
                 <div className='watched-tag'>Watched</div>
-                <section className='delete-play-buttons'> 
-                     <img className='play-button' src={'https://i.stack.imgur.com/X2c0V.png'} onClick={() => this.props.update(this.props.movie.id)}/>
+                <img className='play-button' src={'https://i.stack.imgur.com/X2c0V.png'} onClick={() => this.props.update(this.props.movie.id)}/>
+                <section className='delete-button-section'> 
                     <div className='delete-button delete-watched' onClick={() => this.props.delete(this.props.movie.id)}>X</div>
                 </section>
             </div>
             )}
             else {return(
-                <div className='my-movies'>
+            <div className='my-movies'>
                 <img className='my-poster' src={`https://image.tmdb.org/t/p/w500${this.props.movie.poster_path}`} alt={this.props.movie.title}/>
                 <section className='delete-play-buttons'>
                     <img className='play-button' src={'https://i.stack.imgur.com/X2c0V.png'} onClick={() => this.props.update(this.props.movie.id)}/>
